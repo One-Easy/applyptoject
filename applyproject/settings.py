c7a44e4e72c4b249481a0ec4b987de46c25579fa
One-Easy <bu_bu@kakao.com>
2021-07-28 20:03:16 +0900

    first commit

--- applyproject/settings.py
+++ applyproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-wmn2rgqy7od-xh^3s7f&o@@ok4ac9koy=dpfw-vfenhmuls+_=')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get('DJANGO_DBUG'), True)
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = ['*']
 
